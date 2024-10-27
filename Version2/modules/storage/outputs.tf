07c02615139e03982c52eb8e14c98e935103ec22
StephParc <139220405+StephParc@users.noreply.github.com>
2024-10-27 19:23:56 +0100

    Delete Version2 directory

--- Version2/modules/storage/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.my_storage_account.name
-}
-
-output "blob_name" {
-  value = azurerm_storage_container.blob_container.name
-}
--- Version2/modules/web_app/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "app_service_plan_name" {
-  value = azurerm_app_service_plan.app_service_plan.name
-}
-
-output "webapp_name" {
-  value = azurerm_linux_web_app.app-service.name
-}
